5f1e9115771ef8b9a4064d924b5f4b8216380cea
vijaypuri028 <f4funvijaypuri@gmail.com>
2024-12-05 22:48:58 +0530

    first commit

---
+++ envData.js
@@ -0,0 +1,3 @@
+export const ACCESS_TOKE_PRIVATE_KEY = "Et93eHfFGyvEhALOcc2k"
+export const CORS_ORIGIN = "*"
+export const MONGO_URI = "mongodb+srv://comitymember:vijaycomity%401208@comity.rsd4g.mongodb.net/your-database-name?retryWrites=true&w=majority"
